c851c7bd285e8b9b351b23aa1357af234074bd13
BrianP <brian_poblete@dlsu.edu.ph>
2018-02-28 22:32:20 +0800

    Added ability to perform another transaction

--- ATM.java
+++ ATM.java
@@ -11,25 +11,40 @@ public class ATM{
 	public void useATM() {
 		Scanner sc = new Scanner(System.in);
 		int choice = 0;
+		boolean choiceMade = false;
 		System.out.println("ATM START");
-		System.out.println("Withdraw 	[1]"
-						 + "Deposit  	[2]"
-						 + "Inquire Bal [3]");
 		
-		try {
-			choice = Integer.parseInt(sc.nextLine());
-		} catch (NumberFormatException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 		
-		if(choice == 1) {
-			withdraw();
-		}else if (choice == 2) {
-			deposit();
-		}else if (choice == 3) {
-			inquireBalance();
-		}
+		do {
+			System.out.println("Withdraw 	[1]"
+					 		 + "Deposit  	[2]"
+					 		 + "Inquire Bal [3]");
+			try {
+				choice = Integer.parseInt(sc.nextLine());
+			} catch (NumberFormatException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				System.out.println("You must enter a valid choice.");
+			}
+			
+			if(choice == 1) {
+				withdraw();
+				choiceMade = true;
+			}else if (choice == 2) {
+				deposit();
+				choiceMade = true;
+			}else if (choice == 3) {
+				inquireBalance();
+				choiceMade = true;	
+			}
+			
+			System.out.println("Would you like to continue your transaction? [Y/N]");
+			if(sc.nextLine().equalsIgnoreCase("n"))
+				choiceMade = true;
+			
+		}while(!choiceMade);
+		
+		//printReceipt()
 	}
 	public void withdraw(){
 		
